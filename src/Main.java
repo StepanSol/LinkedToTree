ca712494f0bdb52f7996f4f8312e515fe55b12d7
Stepan <stepan.solovev1994@gmail.com>
2025-02-11 23:40:53 +0300

    LinkedToTree

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,19 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/*
+    При создании HashMap с конструктором по умолчанию(без указания initialCapacity), перестройка
+    в дерево происходит после добавления одиннадцатой пары ключ-значение. Это происходит из-за того,
+    что внутренний ассоциативный массив 2 раза увеличивается(размер по умолчанию - 16), прежде чем
+    начнётся трификация.
+ */
 public class Main {
     public static void main(String[] args){
 
         Map<ClassForMap, Integer> map = new HashMap<>(64);
         List<String> classes = new ArrayList<>();
+
+
 
         for (int i = 1; i < 10; i++) {
             map.put(new ClassForMap(i), i);
